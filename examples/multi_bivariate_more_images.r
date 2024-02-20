569f95228fc33b9e5f8730ef3cbb1ad53534429f
Joel Eliason <joel.eliason119@gmail.com>
2024-02-20 12:31:44 -0700

    more images example.

---
+++ examples/multi_bivariate_more_images.r
@@ -0,0 +1,185 @@
+#renv::activate(".")
+rm(list=ls())
+devtools::load_all()
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+set.seed(2020)
+
+num_images <- 50
+
+SS <- 16 # coord values for jth dimension
+dd <- 2 # spatial dimension
+n <- SS^2 # number of locations
+q <- 4 # number of outcomes
+k <- 2 # number of spatial factors used to make the outcomes
+p <- 1 # number of covariates
+
+xlocs <- seq(0, 1, length.out=SS)
+coords <- expand.grid(list(xlocs, xlocs)) %>%
+  as.data.frame()
+
+clist <- 1:q %>% lapply(function(i) coords %>%
+                          mutate(mv_id=i) %>%
+                          as.matrix())
+
+philist <- c(15, 15) # spatial decay for each factor
+
+# cholesky decomp of covariance matrix
+LClist <- 1:k %>% lapply(function(i) t(chol(
+  #exp(- philist[i] * as.matrix(dist(clist[[i]])))))) #^2 + diag(nrow(clist[[i]]))*1e-5))))
+  bipps:::Cov_matern(clist[[i]], clist[[i]], 1, philist[i], 1.5, 0, T, 10))))
+
+# generating the factors
+WW <- lapply(1:num_images,\(j) {
+  wlist <- 1:k %>% lapply(function(i) LClist[[i]] %*% rnorm(n))
+
+  # factor matrix
+  do.call(cbind, wlist)
+})
+
+
+# factor loadings
+Lambda <- matrix(0, q, ncol(WW[[1]]))
+diag(Lambda) <- runif(k, 1, 2)
+Lambda[lower.tri(Lambda)] <- runif(sum(lower.tri(Lambda)), -1, 1)
+Lambda[2,2] <- .5
+
+# nuggets
+tau.sq <- rep(.01, q)
+TTsq <- matrix(1, nrow=n) %x% matrix(tau.sq, ncol=length(tau.sq))
+# measurement errors
+# double check these
+EE <- ( rnorm(n*length(tau.sq)) %>% matrix(ncol=length(tau.sq)) ) * TTsq^.5
+
+XX <- lapply(1:num_images,\(j) {
+  1:p %>% lapply(function(i) rnorm(n, 0, .1)) %>% do.call(cbind, .)
+})
+Beta <- matrix(rnorm(p*q), ncol=q) * 0
+
+# outcome matrix, fully observed
+linear_predictor <- lapply(1:num_images,\(i) XX[[i]] %*% Beta + WW[[i]] %*% t(Lambda))
+YY_full <- lapply(1:num_images,\(i) {
+  y <- matrix(0, ncol=q, nrow=nrow(linear_predictor[[i]]))
+  y <- lapply(1:q,\(j) {
+    rpois(n, exp(linear_predictor[[i]][,j]))
+  })
+  y <- do.call(cbind,y)
+})
+
+
+# .. introduce some NA values in the outcomes
+YY <- YY_full
+
+#YY[sample(1:n, n/5, replace=FALSE), 1] <- NA
+#YY[sample(1:n, n/5, replace=FALSE), 2] <- NA
+
+
+simdata <- coords %>%
+  cbind(data.frame(Outcome_full=YY_full#,
+                   #Outcome_obs = YY
+                   ))
+
+plotting_data <- FALSE
+if(plotting_data){
+ simdata %>%
+   tidyr::gather(Outcome, Value, -all_of(colnames(coords))) %>%
+   ggplot(aes(Var1, Var2, fill=Value)) +
+   geom_raster() +
+   facet_wrap(Outcome ~., ncol=2, scales="free") +
+   scale_fill_viridis_c()
+}
+
+mcmc_keep <- 10000
+mcmc_burn <- 2000
+mcmc_thin <- 1
+
+
+
+#devtools::load_all()
+set.seed(1)
+mesh_total_time <- system.time({
+  meshout <- multi_bipps(YY, family="poisson", XX, coords, k = 2,
+                      block_size=25,
+                      n_samples = mcmc_keep, n_burn = mcmc_burn, n_thin = mcmc_thin,
+                      n_threads = 2,
+                      starting=list(lambda = Lambda, beta=Beta, phi=15),
+                      prior = list(btmlim= .01, toplim=1e3, phi=c(1, 40), nu=c(1.5, 1.5)),
+                      settings = list(adapting=T, saving=F, ps=F, hmc=0),
+                      verbose=10,
+                      debug=list(sample_beta=T, sample_tausq=F,
+                                 sample_theta=T, sample_w=T, sample_lambda=T,
+                                 verbose=T, debug=F)
+  )})
+
+
+plot_cube <- function(cube_mcmc, q, k, name="Parameter"){
+  par(mar=c(2.5,2,1,1), mfrow=c(q,k))
+  for(i in 1:q){
+    for(j in 1:k){
+      cube_mcmc[i, j,] %>% plot(type='l', main="{name} {i}, {j}" %>% glue::glue())
+    }
+  }
+}
+
+# chain plots
+plot_cube(meshout$theta_mcmc, 1, k, "theta")
+plot_cube(meshout$lambda_mcmc, q, k, "Lambda")
+plot_cube(meshout$beta_mcmc, p, q, "Beta")
+
+# posterior means
+meshout$tausq_mcmc %>% apply(1, mean)
+meshout$lambda_mcmc %>% apply(1:2, mean)
+meshout$beta_mcmc %>% apply(1:2, mean)
+meshout$theta_mcmc %>% apply(1:2, mean)
+
+# process means
+wmesh <- data.frame(meshout$w_mcmc %>% summary_list_mean())
+colnames(wmesh) <- paste0("wmesh_", 1:k)
+# predictions
+ymesh <- data.frame(meshout$yhat_mcmc %>% summary_list_mean())
+colnames(ymesh) <- paste0("ymesh_", 1:q)
+
+mesh_df <-
+  meshout$coordsdata %>%
+  cbind(ymesh) %>%
+  # cbind(wmesh) %>%
+  cbind(YY_obs)
+
+results <- simdata %>% left_join(mesh_df)
+
+# prediction rmse, out of sample
+results %>% filter(!complete.cases(Outcome_obs.1)) %>%
+  with((Outcome_full.1 - ymesh_1)^2) %>% mean() %>% sqrt()
+results %>% filter(!complete.cases(Outcome_obs.2)) %>%
+  with((Outcome_full.2 - ymesh_2)^2) %>% mean() %>% sqrt()
+
+(postmeans <- results %>% dplyr::select(Var1, Var2, ymesh_1) %>%
+    tidyr::gather(Variable, Value, -Var1, -Var2) %>%
+    ggplot(aes(Var1, Var2, fill=Value)) +
+    geom_raster() +
+    facet_wrap(Variable ~ ., ncol= 2) +
+    scale_fill_viridis_c())
+
+res <- mesh_df %>%
+  pivot_longer(-c(x,y),names_pattern = "(pred|obs)_(.*)$",names_to=c(".value","type"))
+
+
+res %>%
+  pivot_longer(c(pred,obs)) %>%
+  ggplot(aes(x,y, fill=value)) +
+  geom_tile() +
+  facet_wrap(name ~ type, ncol= q) +
+  scico::scale_fill_scico(palette = "batlow")
+
+res %>%
+  group_by(type) %>%
+  summarise(rmse=sqrt(mean((pred-obs)^2)))
+
+(lower <- meshout$lambda_mcmc %>%
+    apply(.,1:2,\(x) quantile(x,probs=c(0.025))))
+
+(upper <- meshout$lambda_mcmc %>%
+    apply(.,1:2,\(x) quantile(x,probs=c(0.975))))
