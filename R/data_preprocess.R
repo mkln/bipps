3146d09f964ce90f9e6a96b5649a5b935dd28699
Joel Eliason <joel.eliason119@gmail.com>
2024-08-30 13:43:56 -0600

    add plotting function.

--- R/data_preprocess.R
+++ R/data_preprocess.R
@@ -12,49 +12,44 @@
 create_y_list <- function(X,Y,types,image_ids,nx,ny) {
   # bottom-left corner at 0,0, scale by max X and Y
   df <- data.frame(X = x, Y = y, type = types,image_id = image_ids) %>%
-    group_by(image_id) %>%
-    mutate(X = X - min(X),
+    dplyr::group_by(image_id) %>%
+    dplyr::mutate(X = X - min(X),
            Y = Y - min(Y)) %>%
-    ungroup() %>%
-    mutate(X = X/max(X),
+    dplyr::ungroup() %>%
+    dplyr::mutate(X = X/max(X),
            Y = Y/max(Y))
 
-  total_grid <- expand_grid(gridded_x=1:nx,gridded_y=1:ny)
+  total_grid <- expand.grid(gridded_x=1:nx,gridded_y=1:ny)
 
   counts <- df %>%
-    mutate(gridded_x = floor((nx-1)*X) + 1,
+    dplyr::mutate(gridded_x = floor((nx-1)*X) + 1,
            gridded_y = floor((ny-1)*Y) + 1) %>%
-    count(gridded_x,gridded_y,type,image_id) %>%
-    pivot_wider(names_from = type, values_from = n) %>%
+    dplyr::count(gridded_x,gridded_y,type,image_id) %>%
+    tidyr::pivot_wider(names_from = type, values_from = n) %>%
     replace(is.na(.),0) %>%
-    group_by(image_id) %>%
-    group_modify(~{
+    dplyr::group_by(image_id) %>%
+    dplyr::group_modify(~{
       .x %>%
-        right_join(total_grid, by = c("gridded_x","gridded_y"))
+        dplyr::right_join(total_grid, by = c("gridded_x","gridded_y"))
     }) %>%
-    ungroup() %>%
-    arrange(image_id,gridded_x,gridded_y)
-
-  counts %>%
-    ggplot(aes(gridded_x,gridded_y,fill=factor(granulocytes,ordered=TRUE))) +
-    geom_tile() +
-    facet_wrap(~image_id)
+    dplyr::ungroup() %>%
+    dplyr::arrange(image_id,gridded_x,gridded_y)
 
   y_list <- counts %>%
-    group_by(image_id) %>%
-    group_map(~{
+    dplyr::group_by(image_id) %>%
+    dplyr::group_map(~{
       .x %>%
-        select(-c(gridded_x,gridded_y)) %>%
+        dplyr::select(-c(gridded_x,gridded_y)) %>%
         as.matrix()
     })
 
-  names(y_list) <- levels(image_ids)
+  names(y_list) <- unique(image_ids)
 
   coords <- counts %>%
-    distinct(gridded_x,gridded_y) %>%
-    mutate(x = gridded_x / max(gridded_x),
+    dplyr::distinct(gridded_x,gridded_y) %>%
+    dplyr::mutate(x = gridded_x / max(gridded_x),
            y = gridded_y / max(gridded_y)) %>%
-    select(x,y)
+    dplyr::select(x,y)
 
   list(y_list=y_list,coords=coords)
 }
