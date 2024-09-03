80e1c18b3fc448ec37471d43d42577396440f0b6
Joel Eliason <joel.eliason119@gmail.com>
2024-09-03 09:20:15 -0600

    crc analysis script.

---
+++ CRC_analysis.R
@@ -0,0 +1,111 @@
+rm(list = ls())
+devtools::load_all()
+library(spatstat)
+library(tidyverse)
+
+set.seed(2020)
+
+df_raw <- readr::read_csv("examples/data/CRC_cleaned.csv") %>%
+  # dplyr::mutate(type = as.factor(type)) %>%
+  dplyr::rename(Spot = spots)
+# mutate(Spot = factor(Spot))
+
+
+df_raw %>%
+  distinct(Spot,groups) %>%
+  filter(groups == 1) %>%
+  pull(Spot) -> spots1
+
+df_raw %>%
+  distinct(Spot,groups) %>%
+  filter(groups == 2) %>%
+  pull(Spot) -> spots2
+
+dat1 <- df_raw %>%
+  filter(Spot %in% spots1)
+
+dat2 <- df_raw %>%
+  filter(Spot %in% spots2)
+
+dat1 %>%
+  count(Spot,type) %>%
+  group_by(type) %>%
+  summarise(mn = median(n)) %>%
+  filter(mn > 10) %>%
+  pull(type) -> types1
+
+dat2 %>%
+  count(Spot,type) %>%
+  group_by(type) %>%
+  summarise(mn = median(n)) %>%
+  filter(mn > 10) %>%
+  pull(type) -> types2
+
+
+types_intersect <- intersect(types1,types2)
+
+dat1 <- dat1 %>%
+  filter(type %in% types_intersect)
+
+dat2 <- dat2 %>%
+  filter(type %in% types_intersect)
+
+nx <- ny <- 20
+out1 <- create_y_list(dat1$X,dat1$Y,dat1$type,dat1$Spot,nx,ny)
+y_list1 <- out1$y_list
+coords1 <- out1$coords
+x_list1 <- lapply(y_list1,\(yy) {
+  matrix(0,nrow = nrow(yy),ncol = 1)
+})
+
+out2 <- create_y_list(dat2$X,dat2$Y,dat2$type,dat2$Spot,nx,ny)
+y_list2 <- out2$y_list
+coords2 <- out2$coords
+x_list2 <- lapply(y_list2,\(yy) {
+  matrix(0,nrow = nrow(yy),ncol = 1)
+})
+
+n_samples <- 2000
+n_burnin <- 1000
+n_thin <- 1
+n_threads <- 16
+
+out1 <- multi_bipps(y_list1,
+                    x_list1,
+                    coords1,
+                    k = 4,
+                    family = "poisson",
+                    block_size = 25,
+                    n_samples = n_samples, n_burn = n_burnin, n_thin = n_thin,
+                    n_threads = n_threads,
+                    starting = list(phi = 100),
+                    prior = list(phi = c(0.1, 200)),
+                    settings = list(adapting = T, saving = T, ps = T),
+                    verbose = 10,
+                    debug = list(
+                      sample_beta = T, sample_tausq = F,
+                      sample_theta = T, sample_w = T, sample_lambda = T,
+                      verbose = F, debug = F
+                    ),
+                    just_preprocess = F)
+saveRDS(out1,"out1_CRC_analysis.rds")
+
+out2 <- multi_bipps(y_list2,
+                    x_list2,
+                    coords2,
+                    k = 4,
+                    family = "poisson",
+                    block_size = 25,
+                    n_samples = n_samples, n_burn = n_burnin, n_thin = n_thin,
+                    n_threads = n_threads,
+                    starting = list(phi = 100),
+                    prior = list(phi = c(0.1, 200)),
+                    settings = list(adapting = T, saving = T, ps = T),
+                    verbose = 10,
+                    debug = list(
+                      sample_beta = T, sample_tausq = F,
+                      sample_theta = T, sample_w = T, sample_lambda = T,
+                      verbose = F, debug = F
+                    ),
+                    just_preprocess = F)
+saveRDS(out2,"out2_CRC_analysis.rds")
