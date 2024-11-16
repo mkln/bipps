662f062b9be0879350823f3130fe07548902a88e
Joel Eliason <joel.eliason119@gmail.com>
2024-11-15 19:45:13 -0700

    k2 out1.

--- examples/CRC_analysis_runner.R
+++ examples/CRC_analysis_runner.R
@@ -77,22 +77,22 @@ ny <- ceiling(max_dim[2]/pix_dim)
 # nx <- 20
 # ny <- 20
 
-# out1 <- pixellate_grid(dat1$X,dat1$Y,dat1$type,dat1$Spot,nx,ny)
-# y_list1 <- out1$y_list
-# coords1 <- out1$coords
-# x_list1 <- lapply(y_list1,\(yy) {
-#   matrix(0,nrow = nrow(yy),ncol = 1)
-# })
+out1 <- pixellate_grid(dat1$X,dat1$Y,dat1$type,dat1$Spot,nx,ny)
+y_list1 <- out1$y_list
+coords1 <- out1$coords
+x_list1 <- lapply(y_list1,\(yy) {
+  matrix(0,nrow = nrow(yy),ncol = 1)
+})
 
 # p1 <- plot_y_list(y_list1,coords1)
 # p1
 
-out2 <- pixellate_grid(dat2$X,dat2$Y,dat2$type,dat2$Spot,nx,ny)
-y_list2 <- out2$y_list
-coords2 <- out2$coords
-x_list2 <- lapply(y_list2,\(yy) {
-  matrix(0,nrow = nrow(yy),ncol = 1)
-})
+# out2 <- pixellate_grid(dat2$X,dat2$Y,dat2$type,dat2$Spot,nx,ny)
+# y_list2 <- out2$y_list
+# coords2 <- out2$coords
+# x_list2 <- lapply(y_list2,\(yy) {
+#   matrix(0,nrow = nrow(yy),ncol = 1)
+# })
 
 # p <- plot_y_list(y_list2,coords1)
 # p[[52]]
@@ -102,7 +102,7 @@ n_burnin <- 20000
 n_thin <- 40
 n_threads <- 16
 block_size <- 50
-k <- 3
+k <- 2
 starting <- list(phi = 5)
 # D <- sqrt(max_dim[1]^2 + max_dim[2]^2)
 # a <- -log(0.05) * 3 / D
@@ -110,29 +110,9 @@ prior <- list(phi = c(0.1,10))
 
 chains <- 1
 
-# out1 <- lapply(1:chains,\(i) multi_bipps(y_list1,
-#                     x_list1,
-#                     coords1,
-#                     k = k,
-#                     family = "poisson",
-#                     block_size = block_size,
-#                     n_samples = n_samples, n_burn = n_burnin, n_thin = n_thin,
-#                     n_threads = n_threads,
-#                     starting = starting,
-#                     prior = prior,
-#                     settings = list(adapting = T, saving = T, ps = T),
-#                     verbose = 10,
-#                     debug = list(
-#                       sample_beta = T, sample_tausq = F,
-#                       sample_theta = T, sample_w = T, sample_lambda = T,
-#                       verbose = F, debug = F
-#                     ),
-#                     just_preprocess = F))
-# saveRDS(out1,"out1_chains_CRC_analysis_40k.rds")
-
-out2 <- lapply(1:chains,\(i) multi_bipps(y_list2,
-                    x_list2,
-                    coords2,
+out1 <- lapply(1:chains,\(i) multi_bipps(y_list1,
+                    x_list1,
+                    coords1,
                     k = k,
                     family = "poisson",
                     block_size = block_size,
@@ -148,8 +128,28 @@ out2 <- lapply(1:chains,\(i) multi_bipps(y_list2,
                       verbose = F, debug = F
                     ),
                     just_preprocess = F))
+# saveRDS(out1,"out1_chains_CRC_analysis_40k.rds")
+
+# out2 <- lapply(1:chains,\(i) multi_bipps(y_list2,
+#                     x_list2,
+#                     coords2,
+#                     k = k,
+#                     family = "poisson",
+#                     block_size = block_size,
+#                     n_samples = n_samples, n_burn = n_burnin, n_thin = n_thin,
+#                     n_threads = n_threads,
+#                     starting = starting,
+#                     prior = prior,
+#                     settings = list(adapting = T, saving = T, ps = T),
+#                     verbose = 10,
+#                     debug = list(
+#                       sample_beta = T, sample_tausq = F,
+#                       sample_theta = T, sample_w = T, sample_lambda = T,
+#                       verbose = F, debug = F
+#                     ),
+#                     just_preprocess = F))
 # saveRDS(out2,"out2_chains1_CRC_analysis_40k_k2.rds")
 
-out2_lt <- lapply(out2,\(o) list(theta_mcmc=o$theta_mcmc,lambda_mcmc=o$lambda_mcmc))
+out1_lt <- lapply(out1,\(o) list(theta_mcmc=o$theta_mcmc,lambda_mcmc=o$lambda_mcmc))
 
-saveRDS(out2_lt,"out2_chains1_CRC_analysis_40k_k3_lt.rds")
+saveRDS(out1_lt,"out1_chains1_CRC_analysis_40k_k2_lt.rds")
