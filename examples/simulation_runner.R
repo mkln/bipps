c20d3b1d38df75f083f243d34dfa852167c4a73d
Joel Eliason <joel.eliason119@gmail.com>
2024-12-06 00:39:42 -0700

    larger lambda.

--- examples/simulation_runner.R
+++ examples/simulation_runner.R
@@ -78,7 +78,7 @@ set.seed(2025)
 Lambda <- matrix(0, q, k)
 diag(Lambda) <- runif(k, 0.5, 1)
 Lambda[2,1] <- 0.5
-Lambda[lower.tri(Lambda)] <- runif(sum(lower.tri(Lambda)), -0.7, 0.7)
+# Lambda[lower.tri(Lambda)] <- runif(sum(lower.tri(Lambda)), -0.7, 0.7)
 
 # nuggets
 # tau.sq <- rep(.01, q)
