f4a71a4d23298d0bcaa3ba2878e42cb4ab5164ce
Joel Eliason <joel.eliason119@gmail.com>
2024-12-17 10:40:14 -0700

    varying pixels.

---
+++ examples/simulations_varyingpixels_slurm_runner.R
@@ -0,0 +1,122 @@
+devtools::load_all()
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+args <- commandArgs(trailingOnly = TRUE)
+sim_idx <- as.integer(args[1])
+
+# simset1
+szs <- c(10,30,60)
+
+# mcmc and model settings
+grid <- expand.grid(sz=szs,sim=1:10)
+start_idx <- 1
+file_prefix <- "/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset_varyingpx"
+seed_start <- 26
+
+n_samples <- 1000
+n_burnin <- 5000
+n_thin <- 5
+n_threads <- 4
+block_size <- 50
+starting <- list(phi = 1)
+prior <- list(phi = c(0.1,10))
+phi_range <- c(1,3)
+chains <- 1
+do_plots <- FALSE
+sample_theta <- TRUE
+num_images <- 40
+mu <- -2
+k <- 4
+q <- 10
+
+# simulation settings
+print(sim_idx)
+seed <- seed_start + sim_idx
+nx <- ny <- grid$sz[sim_idx]
+n <- nx*ny
+x_max <- 1919
+y_max <- 1439
+p <- 1
+
+coords <- expand.grid(x=seq(0,x_max,length.out=nx),y=seq(0,y_max,length.out=ny))
+
+coords <- coords / max(x_max,y_max)
+c_mat <- as.matrix(coords)
+d_coords <- as.matrix(dist(c_mat))
+
+save_file <- paste0(file_prefix,"_",sim_idx,".rds")
+save_file_ltb <- paste0(file_prefix,"_",sim_idx,"_ltb.rds")
+
+set.seed(seed)
+philist <- runif(k,phi_range[1],phi_range[2])
+
+LClist <- 1:k %>% lapply(\(i) t(chol(
+  exp(- philist[i] * d_coords)
+)))
+
+set.seed(seed)
+VV <- lapply(1:num_images,\(j) {
+  wlist <- lapply(1:k,\(i) LClist[[i]] %*% rnorm(n))
+
+  # factor matrix
+  do.call(cbind, wlist)
+})
+
+
+# factor loadings
+Lambda <- matrix(0, q, k)
+set.seed(seed)
+diag(Lambda) <- runif(k, 0.5, 1)
+Lambda[lower.tri(Lambda)] <- runif(sum(lower.tri(Lambda)), -0.7, 0.7)
+
+Beta <- matrix(rep(mu,p*q),ncol=q)
+
+x_list <- lapply(1:num_images,\(i) {
+  matrix(1,nrow = n,ncol = p)
+})
+
+WW <- lapply(1:num_images,\(i) {
+  mat <- VV[[i]] %*% t(Lambda) + x_list[[i]] %*% Beta
+  # print(range(exp(mat)))
+  mat
+})
+
+set.seed(seed)
+y_list <- lapply(WW,\(ww) {
+  matrix(rpois(nrow(ww)*ncol(ww),exp(ww)),nrow=nrow(ww),ncol=ncol(ww))
+})
+
+if(do_plots) {
+  fields::image.plot(seq(0,x_max,length.out=nx),seq(0,y_max,length.out=ny),matrix(VV[[10]][,2],nrow = ny,ncol = nx))
+  fields::image.plot(seq(0,x_max,length.out=nx),seq(0,y_max,length.out=ny),matrix(WW[[10]][,2],nrow = ny,ncol = nx))
+  p1 <- plot_y_list(y_list,coords)
+  p1[[34]]
+}
+
+# run bipps
+out <- lapply(1:chains,\(i) multi_bipps(y_list,
+                                        x_list,
+                                        coords,
+                                        k = k,
+                                        family = "poisson",
+                                        block_size = block_size,
+                                        n_samples = n_samples, n_burn = n_burnin, n_thin = n_thin,
+                                        n_threads = n_threads,
+                                        starting = starting,
+                                        prior = prior,
+                                        settings = list(adapting = T, saving = T, ps = T,low_mem=T),
+                                        verbose = 10,
+                                        debug = list(
+                                          sample_beta = T, sample_tausq = F,
+                                          sample_theta = sample_theta, sample_w = T, sample_lambda = T,
+                                          verbose = F, debug = F
+                                        ),
+                                        just_preprocess = F))
+saveRDS(out,save_file)
+
+out_ltb <- lapply(out,\(o) list(theta_mcmc=o$theta_mcmc,lambda_mcmc=o$lambda_mcmc,beta_mcmc=o$beta_mcmc,waic=o$waic))
+
+saveRDS(out_ltb,save_file_ltb)
---
+++ examples/submit_varyingpixels.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name simulation_varyingpixels
+#SBATCH --array=1-30
+#SBATCH --output=pixel_logs/job_%A_%a.out   # Standard output log (%A=job ID, %a=array index)
+#SBATCH --error=pixel_logs/job_%A_%a.err    # Standard error log (%A=job ID, %a=array index)
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4:00:00
+#SBATCH --mem=16G
+#SBATCH --mail-user=joelne@umich.edu
+#SBATCH --mail-type=END,FAIL
+
+module load Rgeospatial/4.3.2-mkl-2024-01-15
+
+Rscript examples/simulations_varyingpixels_slurm_runner.R ${SLURM_ARRAY_TASK_ID}
