ad9f0f2357e49cb69e8a733094a14ffcb3e47016
Joel Eliason <joel.eliason119@gmail.com>
2024-12-20 09:04:05 -0700

    exp value.

---
+++ examples/submit_get_expected_value.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name simulation_get_exp
+#SBATCH --output=logs/job_%A_%a.out   # Standard output log (%A=job ID, %a=array index)
+#SBATCH --error=logs/job_%A_%a.err    # Standard error log (%A=job ID, %a=array index)
+#SBATCH --cpus-per-task=1
+#SBATCH --time=10:00
+#SBATCH --mem=8G
+#SBATCH --mail-user=joelne@umich.edu
+#SBATCH --mail-type=END,FAIL
+
+module load Rgeospatial/4.3.2-mkl-2024-01-15
+
+Rscript examples/get_expected_value.R
