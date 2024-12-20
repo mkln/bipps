7000d4409725592ed844687e9ab40d0807077588
Joel Eliason <joel.eliason119@gmail.com>
2024-12-20 09:01:05 -0700

    exp value.

--- examples/get_expected_value.R
+++ examples/get_expected_value.R
@@ -2,7 +2,18 @@ library(posterior)
 
 sim_idx <- 6
 out_full <- readRDS(paste0("/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset1_group_diff_",sim_idx,".rds"))
-out_exp <- lapply(out_full,\(o) list(v_mcmc=E(o$v_mcmc),
-                                     beta_mcmc=E(o$beta_mcmc)))
+vhat <- out_full[[1]]$v_mcmc
+vhat <- lapply(vhat,\(v) v[1:(nx*ny),])
+arr <- simplify2array(vhat)
+arr <- aperm(arr,perm=c(3,1,2))
+vhat <- posterior::rvar(arr)
+
+beta <- out_full[[1]]$beta_mcmc
+arr <- beta
+arr <- arr[,,seq(1,dim(arr)[3],by=n_thin),drop=FALSE]
+arr <- aperm(arr,perm=c(3,1,2))
+beta <- posterior::rvar(arr)
+out_exp <- lapply(out_full,\(o) list(v_mcmc=E(vhat),
+                                     beta_mcmc=E(beta)))
 
 saveRDS(out_exp,paste0("/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset1_group_diff_",sim_idx,"_exp.rds"))
