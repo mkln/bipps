ceba466b149e72f4d49d7e66948897f4e417828a
Joel Eliason <joel.eliason119@gmail.com>
2025-01-28 15:24:08 -0700

    aggregate sims.

--- examples/simulations_varyingpixels_slurm_runner.R
+++ examples/simulations_varyingpixels_slurm_runner.R
@@ -4,39 +4,59 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-args <- commandArgs(trailingOnly = TRUE)
-sim_idx <- as.integer(args[1])
+# environment settings
+SYSTEM_ENV <- Sys.getenv("SYSTEM_ENV")
+if(SYSTEM_ENV == "laptop") {
+  sim_idx <- 1
+  file_prefix <- "examples/data/"
+  n_threads <- 2
+  n_samples <- 500
+  n_burnin <- 500
+  n_thin <- 1
+  do_plots <- TRUE
+  num_images <- 3
+  k <- 2
+  q <- 3
+} else {
+  args <- commandArgs(trailingOnly = TRUE)
+  sim_idx <- as.integer(args[1])
+  file_prefix <- "/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset_varyingpx"
+  n_threads <- 4
+  n_samples <- 1000
+  n_burnin <- 5000
+  n_thin <- 5
+  do_plots <- FALSE
+  num_images <- 40
+  k <- 4
+  q <- 10
+}
+
 
 # simset1
-szs <- c(15,30,45)
+szs <- c(12,24,48)
 
 # mcmc and model settings
 grid <- expand.grid(sz=szs,sim=1:10)
 start_idx <- 1
-file_prefix <- "/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset_varyingpx"
 seed_start <- 26
 
-n_samples <- 1000
-n_burnin <- 5000
-n_thin <- 5
-n_threads <- 4
 block_size <- 50
 starting <- list(phi = 1)
 prior <- list(phi = c(0.1,10))
 phi_range <- c(1,3)
 chains <- 1
-do_plots <- FALSE
 sample_theta <- TRUE
-num_images <- 40
-mu <- -0.5
-k <- 4
-q <- 10
+mu <- -3
 
 # simulation settings
 print(sim_idx)
-seed <- seed_start + sim_idx
+sim <- grid$sim[sim_idx]
+seed <- seed_start + sim
 nx <- ny <- grid$sz[sim_idx]
 min_nx <- min_ny <- min(grid$sz)
+max_nx <- max_ny <- max(grid$sz)
+
+max_n <- max_nx*max_ny
 n <- nx*ny
 x_max <- 1919
 y_max <- 1439
@@ -49,7 +69,7 @@ max_px_area <- px_minx * px_miny
 size_factor <- px_area / max_px_area
 p <- 1
 
-coords <- expand.grid(x=seq(0,x_max,length.out=nx),y=seq(0,y_max,length.out=ny))
+coords <- expand.grid(x=seq(0,x_max,length.out=max_nx),y=seq(0,y_max,length.out=max_ny))
 
 coords <- coords / max(x_max,y_max)
 c_mat <- as.matrix(coords)
@@ -67,12 +87,11 @@ LClist <- 1:k %>% lapply(\(i) t(chol(
 
 set.seed(seed)
 VV <- lapply(1:num_images,\(j) {
-  wlist <- lapply(1:k,\(i) LClist[[i]] %*% rnorm(n))
+  vlist <- lapply(1:k,\(i) LClist[[i]] %*% rnorm(max_n))
 
   # factor matrix
-  do.call(cbind, wlist)
+  do.call(cbind, vlist)
 })
-
 
 # factor loadings
 Lambda <- matrix(0, q, k)
@@ -83,31 +102,44 @@ Lambda[lower.tri(Lambda)] <- runif(sum(lower.tri(Lambda)), -0.7, 0.7)
 Beta <- matrix(rep(mu,p*q),ncol=q)
 
 x_list <- lapply(1:num_images,\(i) {
-  matrix(1,nrow = n,ncol = p)
+  matrix(1,nrow = max_n,ncol = p)
 })
 
 WW <- lapply(1:num_images,\(i) {
   mat <- VV[[i]] %*% t(Lambda) + x_list[[i]] %*% Beta
-  # print(range(exp(mat)))
+  print(range(exp(mat)))
   mat
 })
 
 set.seed(seed)
-y_list <- lapply(WW,\(ww) {
-  matrix(rpois(nrow(ww)*ncol(ww),exp(ww)*size_factor),nrow=nrow(ww),ncol=ncol(ww))
+y_agg <- lapply(1:num_images,\(i) {
+  yy_list <- lapply(1:k,\(j) {
+    mat <- matrix(WW[[i]][,j],nrow = max_ny,ncol = max_nx)
+    mat <- matrix(rpois(nrow(mat)*ncol(mat),exp(mat)),nrow=nrow(mat),ncol=ncol(mat))
+    r <- raster::raster(mat)
+    r_sub <- raster::aggregate(r,fact = max_nx / nx, fun = sum, expand = FALSE)
+    vec_sub <- c(raster::as.matrix(r_sub))
+  })
+  yy <- do.call(cbind,yy_list)
+})
+agg_coords <- expand.grid(x=seq(0,x_max,length.out=nx),y=seq(0,y_max,length.out=ny))
+agg_coords <- agg_coords / max(x_max,y_max)
+
+x_agg <- lapply(1:num_images,\(i) {
+  matrix(1,nrow = n,ncol = p)
 })
 
 if(do_plots) {
-  fields::image.plot(seq(0,x_max,length.out=nx),seq(0,y_max,length.out=ny),matrix(VV[[10]][,2],nrow = ny,ncol = nx))
-  fields::image.plot(seq(0,x_max,length.out=nx),seq(0,y_max,length.out=ny),matrix(WW[[10]][,2],nrow = ny,ncol = nx))
-  p1 <- plot_y_list(y_list,coords)
-  p1[[34]]
+  fields::image.plot(seq(0,x_max,length.out=max_nx),seq(0,y_max,length.out=max_ny),matrix(VV[[1]][,2],nrow = max_ny,ncol = max_nx))
+  fields::image.plot(seq(0,x_max,length.out=max_nx),seq(0,y_max,length.out=max_ny),matrix(WW[[1]][,2],nrow = max_ny,ncol = max_nx))
+  p1 <- plot_y_list(y_agg,agg_coords)
+  print(p1[[1]])
 }
 
 # run bipps
-out <- lapply(1:chains,\(i) multi_bipps(y_list,
-                                        x_list,
-                                        coords,
+out <- lapply(1:chains,\(i) multi_bipps(y_agg,
+                                        x_agg,
+                                        agg_coords,
                                         k = k,
                                         family = "poisson",
                                         block_size = block_size,
