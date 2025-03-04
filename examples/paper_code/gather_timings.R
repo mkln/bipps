ef9b848c7b7e66804f1242e8ebc769a4cdc866ec
Joel Eliason <joel.eliason119@gmail.com>
2025-03-04 16:03:44 -0700

    get timings.

--- examples/paper_code/gather_timings.R
+++ examples/paper_code/gather_timings.R
@@ -1,7 +1,7 @@
 sim_idx <- 1:30
 
-l_k <- unlist(lapply(sim_idx,\(i) readRDS(paste0("timings_varyingk_",sim_idx,".rds"))))
-l_px <- unlist(lapply(sim_idx,\(i) readRDS(paste0("timings_varyingpx_",sim_idx,".rds"))))
+l_k <- unlist(lapply(sim_idx,\(i) readRDS(paste0("timings_varyingk_",i,".rds"))))
+l_px <- unlist(lapply(sim_idx,\(i) readRDS(paste0("timings_varyingpx_",i,".rds"))))
 
 df <- data.frame(sim_idx=c(sim_idx,sim_idx),timing=c(l_k,l_px),type=c(rep("k",30),rep("px",30)))
 
