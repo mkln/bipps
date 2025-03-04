327b0cbe6b0f4ee8ebb105857886ba6aff07bbee
Joel Eliason <joel.eliason119@gmail.com>
2025-03-04 15:46:53 -0700

    get timings.

--- examples/paper_code/get_timings.R
+++ examples/paper_code/get_timings.R
@@ -2,8 +2,8 @@
 args <- commandArgs(trailingOnly = TRUE)
 sim_idx <- as.integer(args[1])
 
-out_full <- readRDS(paste0("/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset_varyingk_",sim_idx,".rds"))
+out_full <- readRDS(paste0("/nfs/turbo/umms-ukarvind/joelne/bipps_simulations/out_simset_varyingpx_",sim_idx,".rds"))
 
 timing <- unlist(lapply(out_full,\(o) o$mcmc_time))
 
-saveRDS(timing,paste0("timings_varyingk_",sim_idx,".rds"))
+saveRDS(timing,paste0("timings_varyingpx_",sim_idx,".rds"))
